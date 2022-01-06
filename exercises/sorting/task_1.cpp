83eed9a71ba1ff545e9a7f38f7cc716acd94f1d5
Alexander Garustovich <yukigaru@gmail.com>
2022-01-06 20:50:49 +0300

    feat: add first sorting exercise

---
+++ exercises/sorting/task_1.cpp
@@ -0,0 +1,20 @@
+/*
+Task:
+- write a function that returns true if the given sequence is sorted.
+
+plus: add your own tests.
+plus: you have a STL-based implementation and a manual variant.
+plus: support for a reverse-sorted sequences too.
+*/
+#include <iostream>
+#include <vector>
+
+template <typename Iter>
+bool is_sorted(Iter begin, Iter end) {
+  // TODO: implement
+  return false;
+}
+
+int main() {
+  return 0;
+}
