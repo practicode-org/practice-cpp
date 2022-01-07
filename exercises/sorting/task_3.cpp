3608307a54a6b106782fef43b7924aa408b2a84c
Alexander Garustovich <yukigaru@gmail.com>
2022-01-07 19:01:58 +0300

    feat: add exercise sorting_3: sorting in different containers

---
+++ exercises/sorting/task_3.cpp
@@ -0,0 +1,88 @@
+/*
+Task:
+1) print elements of different containers in ascending order. You may change them by calling `sort` if needed.
+*/
+#include <vector>
+#include <deque>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include "gtest/gtest.h"
+
+TEST(AscendingOrder, Vector) {
+  // Note: this one is already done
+  std::vector<int> arr = {6, 5, 4, 1, 3, 2};
+  
+  std::sort(std::begin(arr), std::end(arr));
+
+  testing::internal::CaptureStdout();
+
+  for (auto item : arr) {
+    std::cout << item << " ";
+  }
+
+  EXPECT_EQ(testing::internal::GetCapturedStdout(), "1 2 3 4 5 6 ");
+}
+
+TEST(AscendingOrder, CArray) {
+  int arr[] = {6, 5, 4, 1, 3, 2};
+
+  // TODO: sorting
+
+  testing::internal::CaptureStdout();
+
+  // TODO: printing 
+  
+  EXPECT_EQ(testing::internal::GetCapturedStdout(), "1 2 3 4 5 6 ");
+}
+
+TEST(AscendingOrder, Deque) {
+  std::deque<int> arr = {6, 5, 4, 1, 3, 2};
+
+  testing::internal::CaptureStdout();
+
+  EXPECT_EQ(testing::internal::GetCapturedStdout(), "1 2 3 4 5 6 ");
+}
+
+TEST(AscendingOrder, List) {
+  std::list<int> arr = {6, 5, 4, 1, 3, 2};
+
+  testing::internal::CaptureStdout();
+
+  EXPECT_EQ(testing::internal::GetCapturedStdout(), "1 2 3 4 5 6 ");
+}
+
+TEST(AscendingOrder, Map) {
+  std::map<int, int> arr = {{6, 600},
+    {5, 500},
+    {4, 400},
+    {1, 100},
+    {3, 300},
+    {2, 200}};
+
+  testing::internal::CaptureStdout();
+  
+  // print only keys
+  
+  EXPECT_EQ(testing::internal::GetCapturedStdout(), "1 2 3 4 5 6 ");
+}
+
+TEST(AscendingOrder, UnorderedMap) {
+  std::unordered_map<int, int> arr = {{6, 600},
+    {5, 500},
+    {4, 400},
+    {1, 100},
+    {3, 300},
+    {2, 200}};
+
+  testing::internal::CaptureStdout();
+  
+  // print only keys
+  
+  EXPECT_EQ(testing::internal::GetCapturedStdout(), "1 2 3 4 5 6 ");
+}
+
+int main() {
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
+}
